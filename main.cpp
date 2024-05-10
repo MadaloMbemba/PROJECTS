2cffff58da6a1d430167a057204b9babd074141a
madalombemba <bsc-inf-20-2@unima.ac.mw>
2024-05-10 14:10:53 +0200

    Main.cpp program for a rectangle header

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "RECTANGLE.h"
+int main()
+class Rectangle MyRectangle;
+MyRectangle.setlength(10.0);
+MYRectangle.setwidith(5.0);
+float = MyRectangle.CalculateTheArea();
+cout<<"Area of the rectangle:" << area <<endl;
